6ba9344a8ad863f900bb8d80f1140030fc320e73
vaishali31verma <gadaiyavaishali31@gmail.com>
2024-05-23 00:02:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GoogleAuth from './GoggleAuth';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleAuth />
+    </div>
+  );
+}
+
+export default App;
