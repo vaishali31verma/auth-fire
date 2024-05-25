704f93329b15ca94af89d8b6268177061dffe125
vaishali31verma <gadaiyavaishali31@gmail.com>
2024-05-25 10:54:37 +0530

    env added

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDp0YagVgeNQXpmQCV7qyzEZ8DwquGj3h4",
-  authDomain: "auth-example-24715.firebaseapp.com",
-  projectId: "auth-example-24715",
-  storageBucket: "auth-example-24715.appspot.com",
-  messagingSenderId: "766819906578",
-  appId: "1:766819906578:web:f5a08923573a3e6dd594cb",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
