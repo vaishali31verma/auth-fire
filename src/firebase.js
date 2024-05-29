e9ff0a2308750c59793f98f13e3ae0dbd0fe7fd1
kapil sharma <erkapilsharma7@gmail.com>
2024-05-29 14:41:06 +0530

    env fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAJxCfY2VEwKVdBAHY1Chsts9NKJomSPro",
+  authDomain: "authdemo-8de3e.firebaseapp.com",
+  projectId: "authdemo-8de3e",
+  storageBucket: "authdemo-8de3e.appspot.com",
+  messagingSenderId: "599976159637",
+  appId: "1:599976159637:web:c2a632fcb027ee80b06e9c",
+  measurementId: "G-CCWWJBNLMV"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,25 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJxCfY2VEwKVdBAHY1Chsts9NKJomSPro",
+  authDomain: "authdemo-8de3e.firebaseapp.com",
+  projectId: "authdemo-8de3e",
+  storageBucket: "authdemo-8de3e.appspot.com",
+  messagingSenderId: "599976159637",
+  appId: "1:599976159637:web:c2a632fcb027ee80b06e9c",
+  measurementId: "G-CCWWJBNLMV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
